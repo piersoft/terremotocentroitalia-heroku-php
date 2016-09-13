51af6f3055754d9489152f4fbdfefb750e9669aa
terremotocentro <terremotocentroita@gmail.com>
2016-09-13 22:51:25 +0200

    Ottimizza webhooks.php
    - Non scaricare da zero il repository git, ma riusa la cartella se
      presente
    - Reso più robusto il sistema di locking
    - Evito di scrivere boiate se non riesco ad eseguire uno script

--- webhooks.php
+++ webhooks.php
@@ -1,43 +1,57 @@
 <?php
-	define('GITHUB_REPO', 'git@github.com:emergenzeHack/terremotocentro.git');
+	define('GITHUB_REPO', 'https://' . getenv('GITHUB_USERNAME') . ':' . getenv('GITHUB_PASSWORD') . '@github.com/emergenzeHack/terremotocentro');
 	define('GITLAB_REPO', 'https://' . getenv('GITLAB_AUTH') . '@gitlab.com/emergenzeHack/terremotocentro');
+
+	function is_empty_dir($dir) {
+		return (($files = @scandir($dir)) && count($files) <= 2);
+	}
+
+	function exec_log($cmd) {
+		exec($cmd, $out, $ret);
+		error_log(join('\n', $out));
+		return !$ret;
+	}
+
+	function cleanup() {
+		rmdir('/tmp/terremotocentro.lock');
+	}
 
 	$token = getenv('WEBHOOKS_TOKEN');
 //	if (!$token || $_GET['token'] != $token || ($_GET['type'] != 'csv' && $_GET['type'] != 'sync')) { FIXME
 	if (!$token || $_GET['token'] != $token || $_GET['type'] != 'issue') {
 		exit;
 	}
-	$ssh_key = getenv('GITHUB_TERREMOTOCENTRO_SSH_KEY');
-	$home = getenv('HOME');
-	mkdir($home . '/.ssh', 0700);
-	file_put_contents($home . '/.ssh/id_rsa', $ssh_key);
-	file_put_contents($home . '/.ssh/config', "Host github.com\n\tStrictHostKeyChecking no\n");
-	chmod($home . '/.ssh/id_rsa', 0600);
 
-	// Poor man™ locking
-	while (!mkdir('/tmp/terremotocentro')) {
+	$i = 0;
+	// Poor man™ locking (funziona solo se hai un worker, come nel caso di Heroku free)
+	while (!@mkdir('/tmp/terremotocentro.lock')) {
 		sleep(1);
+		$i++;
+		if ($i && ($i % 5) == 0)
+			error_log("Locked for $i seconds");
+	}
+	register_shutdown_function('cleanup');
+
+	if (is_dir('/tmp/terremotocentro') && !is_empty_dir('/tmp/terremotocentro')) {
+		chdir('/tmp/terremotocentro');
+		exec_log('git fetch origin master && git reset --hard origin/master');
+	} else {
+		exec_log('git clone ' . GITHUB_REPO . ' /tmp/terremotocentro');
+		chdir('/tmp/terremotocentro');
+		exec_log('git config user.name terremotocentro && git config user.email terremotocentroita@gmail.com');
 	}
 	// Scarico le pagine da Google Spreadsheet e le converto in CSV
 	if ($_GET['type'] == 'csv') {
-		passthru('git clone ' . GITHUB_REPO . ' /tmp/terremotocentro 2>&1');
-		chdir('/tmp/terremotocentro');
-		passthru('git config user.name terremotocentro && git config user.email terremotocentroita@gmail.com');
-		passthru('bash scripts/csvupdate.sh 2>&1');
+		exec_log('bash scripts/csvupdate.sh');
 	// Scarico le issue da GitHub e le converto in CSV
 	} elseif ($_GET['type'] == 'issue') {
-		passthru('git clone ' . GITHUB_REPO . ' /tmp/terremotocentro 2>&1');
-		chdir('/tmp/terremotocentro');
-		passthru('git config user.name terremotocentro && git config user.email terremotocentroita@gmail.com');
-		passthru('python2 scripts/github2CSV.py _data/issues.csv 2>&1 && sed -i \'s/\r$//g\' _data/issues.csv');
-		passthru('git add _data/issues.csv');
-		passthru('git commit -m "auto issues CSV update ' . date('c') . '"');
-		passthru('git push origin master');
+		exec_log('python2 scripts/github2CSV.py _data/issues.csv && sed -i \'s/\r$//g\' _data/issues.csv') or exit(0);
+		exec_log('git add _data/issues.csv');
+		exec_log('git commit -m "auto issues CSV update ' . date('c') . '"');
+		exec_log('git push origin master');
 	// Sincronizzo GitLab con GitHub
 	} elseif ($_GET['type'] == 'sync') {
-		passthru('git clone --mirror ' . GITHUB_REPO . ' /tmp/terremotocentro 2>&1');
 		chdir('/tmp/terremotocentro');
-		passthru('git push --all ' . GITLAB_REPO . ' 2>&1');
+		exec_log('git push ' . GITLAB_REPO . ' master:master');
 	}
-	exec('rm -rf /tmp/terremotocentro');
 ?>
