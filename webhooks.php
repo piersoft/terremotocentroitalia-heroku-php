6afedbc35af333c1835d55bf2b6bae0d6e0377d7
terremotocentro <terremotocentroita@gmail.com>
2016-09-14 11:26:11 +0200

    webhooks.php: Non serializzare il sync con la generazione del CSV delle issue

--- webhooks.php
+++ webhooks.php
@@ -12,46 +12,49 @@
 		return !$ret;
 	}
 
-	function cleanup() {
-		rmdir('/tmp/terremotocentro.lock');
+	function cleanup($type) {
+		rmdir("/tmp/terremotocentro.lock.$type");
 	}
 
 	$token = getenv('WEBHOOKS_TOKEN');
-//	if (!$token || $_GET['token'] != $token || ($_GET['type'] != 'csv' && $_GET['type'] != 'sync')) { FIXME
-	if (!$token || $_GET['token'] != $token || ($_GET['type'] != 'sync' && $_GET['type'] != 'issue')) {
+	$type = $_GET['type'];
+//	if (!$token || $_GET['token'] != $token || ($type != 'csv' && $type != 'sync')) { FIXME
+	if (!$token || $_GET['token'] != $token || ($type != 'sync' && $type != 'issue')) {
 		exit;
 	}
 
+	$git_dir="/tmp/terremotocentro.$type";
+
 	$i = 0;
 	// Poor man™ locking (funziona solo se hai un worker, come nel caso di Heroku free)
-	while (!@mkdir('/tmp/terremotocentro.lock')) {
+	while (!@mkdir("/tmp/terremotocentro.lock.$type")) {
 		sleep(1);
 		$i++;
 		if ($i && ($i % 5) == 0)
 			error_log("Locked for $i seconds");
 	}
-	register_shutdown_function('cleanup');
+	register_shutdown_function('cleanup', $type);
 
-	if (is_dir('/tmp/terremotocentro') && !is_empty_dir('/tmp/terremotocentro')) {
-		chdir('/tmp/terremotocentro');
+	if (is_dir($git_dir) && !is_empty_dir($git_dir)) {
+		chdir($git_dir);
 		exec_log('git fetch origin master && git reset --hard origin/master');
 	} else {
-		exec_log('git clone ' . GITHUB_REPO . ' /tmp/terremotocentro');
-		chdir('/tmp/terremotocentro');
+		exec_log('git clone ' . GITHUB_REPO . " $git_dir");
+		chdir($git_dir);
 		exec_log('git config user.name terremotocentro && git config user.email terremotocentroita@gmail.com');
 	}
 	// Scarico le pagine da Google Spreadsheet e le converto in CSV
-	if ($_GET['type'] == 'csv') {
+	if ($type == 'csv') {
 		exec_log('bash scripts/csvupdate.sh');
 	// Scarico le issue da GitHub e le converto in CSV
-	} elseif ($_GET['type'] == 'issue') {
+	} elseif ($type == 'issue') {
 		exec_log('python2 scripts/github2CSV.py _data/issues.csv && sed -i \'s/\r$//g\' _data/issues.csv') or exit(0);
 		exec_log('git add _data/issues.csv');
 		exec_log('git commit -m "auto issues CSV update ' . date('c') . '"');
 		exec_log('git push origin master');
 	// Sincronizzo GitLab con GitHub
-	} elseif ($_GET['type'] == 'sync') {
-		chdir('/tmp/terremotocentro');
+	} elseif ($type == 'sync') {
+		chdir($git_dir);
 		exec_log('git push ' . GITLAB_REPO . ' master:master');
 	}
 ?>
